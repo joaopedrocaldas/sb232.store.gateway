c71876ce9629f3b8a764e7b1ec3fd18a686e599d
joaopedrocaldas <jpecaldas@gmail.com>
2023-11-10 00:10:23 -0300

    a

--- src/main/java/store/gateway/GatewayResource.java
+++ src/main/java/store/gateway/GatewayResource.java
@@ -5,37 +5,38 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class GatewayResource {
 
     @GetMapping("/info")
     public ResponseEntity<Map<String, String>> version() {
         return new ResponseEntity<Map<String, String>>(
-            Map.ofEntries(
-                Map.entry("os.arch", System.getProperty("os.arch")),
-                Map.entry("os.name", System.getProperty("os.name")),
-                Map.entry("os.version", System.getProperty("os.version")),
-                Map.entry("file.separator", System.getProperty("file.separator")),
-                Map.entry("java.class.path", System.getProperty("java.class.path")),
-                Map.entry("java.home", System.getProperty("java.home")),
-                Map.entry("java.vendor", System.getProperty("java.vendor")),
-                Map.entry("java.vendor.url", System.getProperty("java.vendor.url")),
-                Map.entry("java.version", System.getProperty("java.version")),
-                Map.entry("line.separator", System.getProperty("line.separator")),
-                Map.entry("path.separator", System.getProperty("path.separator")),
-                Map.entry("user.dir", System.getProperty("user.dir")),
-                Map.entry("user.home", System.getProperty("user.home")),
-                Map.entry("user.name", System.getProperty("user.name")),
-                Map.entry("jar", new java.io.File(
-                    GatewayApplication.class.getProtectionDomain()
-                        .getCodeSource()
-                        .getLocation()
-                        .getPath()
-                    ).toString())
-            ), HttpStatus.OK
-        );
-    }    
-    
+                Map.ofEntries(
+                        Map.entry("os.arch", System.getProperty("os.arch")),
+                        Map.entry("os.name", System.getProperty("os.name")),
+                        Map.entry("os.version", System.getProperty("os.version")),
+                        Map.entry("file.separator", System.getProperty("file.separator")),
+                        Map.entry("java.class.path", System.getProperty("java.class.path")),
+                        Map.entry("java.home", System.getProperty("java.home")),
+                        Map.entry("java.vendor", System.getProperty("java.vendor")),
+                        Map.entry("java.vendor.url", System.getProperty("java.vendor.url")),
+                        Map.entry("java.version", System.getProperty("java.version")),
+                        Map.entry("line.separator", System.getProperty("line.separator")),
+                        Map.entry("path.separator", System.getProperty("path.separator")),
+                        Map.entry("user.dir", System.getProperty("user.dir")),
+                        Map.entry("user.home", System.getProperty("user.home")),
+                        Map.entry("user.name", System.getProperty("user.name")),
+                        Map.entry("jar", new java.io.File(
+                                GatewayApplication.class.getProtectionDomain()
+                                        .getCodeSource()
+                                        .getLocation()
+                                        .getPath())
+                                .toString())),
+                HttpStatus.OK);
+    }
+
 }
